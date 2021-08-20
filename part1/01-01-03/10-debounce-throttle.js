8fc0958bd5683d7156313bd6346bc18c01c07437
“panhe” <1158817942@qq.com>
2021-08-20 12:39:08 +0800

    节流

--- part1/01-01-03/10-debounce-throttle.js
+++ part1/01-01-03/10-debounce-throttle.js
@@ -7,6 +7,7 @@
  * 	输入的模糊匹配
  * 	轮播图切换
  * 	点击操作
+ *  键盘操作
  * 	。。。。
  * 	浏览器默认情况下都会有自己的监听事件间隔（4 -6ms），如果检测到多次事件监听执行，那么就会造成不必要的资源的浪费
  * 	
@@ -15,3 +16,59 @@
  * 	节流：对于高频率操作按照我们设置的频率来触发
  * 		
  * */
+
+function debounce(handle, wait, immediate) { // 防抖
+    if (typeof handle !== 'function') throw new Error('handle must be funtion')
+    if (typeof wait == 'boolean') {
+        wait = 1000
+        immediate = wait
+    }
+    if(typeof wait === 'undefined') {
+        wait = 1000
+        immediate = false
+    }
+    if(typeof immediate === 'undefined') {
+        immediate = false
+    }
+    let timer = null
+    return function (...args) {
+        const self = this
+        if(immediate) {
+            if(!timer) {
+                handle.call(self, ...args)
+                timer = setTimeout(() => {
+                    timer = null
+                }, wait)
+            }
+        } else {
+            clearTimeout(timer)
+            timer = setTimeout(handle.bind(self, ...args), wait)
+        }
+    }
+}
+
+function throttle (handle, wait) { // 节流
+    if (typeof handle !== 'function') throw new Error('handle must be funtion')
+    if(typeof wait === 'undefined') {
+        wait = 1000
+    }
+    let pre = 0
+    let timer = null
+    return function(...args) {
+        const self = this
+        const now = new Date()
+        const interval = wait - (now - pre)
+        if(interval <=0) { // 处于低频操作中
+            clearTimeout(timer)
+            handle.call(self, ...args)
+            pre = new Date()
+        } else { // 处于高频操作中
+            // 确保最后一次执行
+            clearTimeout(timer)
+            timer = setTimeout(() => {
+                handle.call(self, ...args)
+                pre = new Date()
+            }, interval)
+        }
+    }
+}
