716898e02ccde0b193109c332c2a3500fc05d131
“panhe” <1158817942@qq.com>
2021-09-09 17:46:47 +0800

    part2‘ ’

---
+++ part2/02-01-03/gulp.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	menus: [
+		{
+			
+			name: 'Home',
+			icon: 'aperture',
+			link: 'index.html'
+		},
+		{
+			name: 'About',
+			icon: 'aperture',
+			link: 'about.html'
+		},
+	],
+	pkg: require('./package.json'),
+	date: new Date(),
+}
--- part2/02-01-03/gulpfile.js
+++ part2/02-01-03/gulpfile.js
@@ -1,4 +1,6 @@
 // gulp 的入口文件
+// gulp的核心工作原理：
+// gulp构建过程中包括三个过程： 输入（读取流） --> 加工（转换流） --> 输出（写入流）
 
 // const { series, parallel } = require('gulp')
 
@@ -39,19 +41,55 @@
 // exports.default = series(foo, sec)
 // exports.default = parallel(foo, sec) 
 
-const fs = require('fs')
-const { Transform } = require('stream')
+// const fs = require('fs')
+// const { Transform } = require('stream')
 
-exports.default = (cb) => {
-	const read = fs.createReadStream('test.css')
-	const write = fs.createWriteStream('test.min.css')
-	const transform = new Transform({
-		transform: (chunk, encoding, callback) => {
-			const input = chunk.toString()
-			const output = input.replace(/\s+/g, '').replace(/\/\*.+?\*\//g,'')
-			callback(null, output)
-		}
-	})
-	read.pipe(transform).pipe(write)
-	cb()
-}
+// exports.default = (cb) => {
+// 	const read = fs.createReadStream('test.css')
+// 	const write = fs.createWriteStream('test.min.css')
+// 	const transform = new Transform({
+// 		transform: (chunk, encoding, callback) => {
+// 			const input = chunk.toString()
+// 			const output = input.replace(/\s+/g, '').replace(/\/\*.+?\*\//g,'')
+// 			callback(null, output)
+// 		}
+// 	})
+// 	read.pipe(transform).pipe(write)
+// 	cb()
+// }
+
+// const { src, dest } = require('gulp')
+// const cleanCss = require('gulp-clean-css')
+// const rename = require('gulp-rename')
+
+// exports.default = () => {
+// 	return src('./test.css')
+// 		.pipe(cleanCss())
+// 		.pipe(rename({
+// 			extname: '.min.css'
+// 		}))
+// 		.pipe(dest('dist'))
+// }
+
+// -----------------------------核心工作 ------------------------
+console.log('JSON.stringify(require("gulp-cli"))')
+module.exports = require('gulp-clis')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
